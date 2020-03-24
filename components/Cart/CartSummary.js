56047f631e2f64a36e7493d0c0b63359194f37ff
AkiraKane <vampire1987116hm@gmail.com>
2020-03-24 01:11:18 -0700

    first commit

---
+++ components/Cart/CartSummary.js
@@ -0,0 +1,47 @@
+import { Button, Segment, Divider } from "semantic-ui-react";
+import React from "react";
+import calculateCartTotal from "../../utils/calculateCartTotal";
+import StripeCheckout from "react-stripe-checkout";
+
+function CartSummary({ products, handleCheckout, success }) {
+  const [isCartEmpty, setCartEmpty] = React.useState(false);
+  const [cartAmount, setCartAmount] = React.useState(0);
+  const [stripeAmount, setStripeAmount] = React.useState(0);
+
+  React.useEffect(() => {
+    const { cartTotal, stripeTotal } = calculateCartTotal(products);
+    setCartAmount(cartTotal);
+    setStripeAmount(stripeTotal);
+    setCartEmpty(products.length === 0);
+  }, [products]);
+  return (
+    <>
+      <Divider />
+      <Segment clearing size="large">
+        <strong>Sub total: </strong> ${cartAmount}
+        <StripeCheckout
+          name="React Reserve"
+          amount={stripeAmount}
+          image={products.length > 0 ? products[0].product.mediaUrl : ""}
+          currency="USD"
+          shippingAddress={true}
+          billingAddress={true}
+          zipCode={true}
+          token={handleCheckout}
+          stripeKey="pk_test_LNI6617lYECQi5ZiWevUZrrV0096W31Dpr"
+          triggerEvent="onClick"
+        >
+          <Button
+            icon="cart"
+            disabled={isCartEmpty || success}
+            color="teal"
+            floated="right"
+            content="Checkout"
+          />
+        </StripeCheckout>
+      </Segment>
+    </>
+  );
+}
+
+export default CartSummary;
---
+++ pages/api/products.js
@@ -0,0 +1,28 @@
+// const express = require("express")   old way
+// import express from "express"  new way
+
+// import products from "../../static/products.json";
+import connectDb from "../../utils/connectDb";
+import Product from "../../models/Product";
+
+connectDb();
+
+export default async (req, res) => {
+  const { page, size } = req.query;
+  // convert querystring values to numbers
+  const pageNum = Number(page);
+  const pageSize = Number(size);
+  let products = [];
+  const totalDocs = await Product.countDocuments();
+  const totalPages = Math.ceil(totalDocs / pageSize);
+  if (pageNum === 1) {
+    products = await Product.find().limit(pageSize);
+  } else {
+    const skips = pageSize * (pageNum - 1);
+    products = await Product.find()
+      .skip(skips)
+      .limit(pageSize);
+  }
+
+  res.status(200).json({ products, totalPages });
+};
